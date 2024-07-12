11c6f8bf012f7e248f3052d29657da50b121eda8
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-07-12 21:12:49 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class ADD {
+    public static void main(String[] args) {
+
+       Scanner Scanner = new Scanner(System.in);
+      
+       int number1 = Scanner.nextInt();
+
+       int number2 = Scanner.nextInt();
+
+       int sum =number1+number2;
+
+       System.out.println(sum);
+
+       Scanner.close();
+
+    }
+}
