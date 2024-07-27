7fde6d91badbcf4ca02849908233370189cec856
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-07-27 11:42:59 +0530

    Create typecasting.java

---
+++ DAY2/typecasting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class typeconv {
+public static void main(string args[]){
+   scanner scanner = new scanner;
+  float marks = 99.9999f:
+    int marks2 = (int)marks;
+  //type casting
+  System.out.println(marks2); 
+}
+}
