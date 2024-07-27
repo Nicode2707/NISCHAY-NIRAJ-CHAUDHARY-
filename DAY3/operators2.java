00afbf076745e49d27c1419d271c274be7c34e0f
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-07-27 12:26:25 +0530

    Create operators2.java

---
+++ DAY3/operators2.java
@@ -0,0 +1,13 @@
+public class operators2{
+  public static void main(string args[])
+  int a =10;
+  int b= a++;
+  //pre incremet
+  /*  
+  1.value use
+  2.value change 
+  */
+  system.out.println(a);
+  system.out.println(b);
+}
+}
