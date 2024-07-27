958e1f3c51bab0c07025f8467203ea91bd92c58a
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-07-27 12:25:30 +0530

    Create operator.java

---
+++ DAY3/operator.java
@@ -0,0 +1,13 @@
+public class operator{
+  public static void main(string args[])
+  int a =10;
+  int b= ++a;
+  //pre incremet
+  /*  
+  1.value change
+  2.value use 
+  */
+  system.out.println(a);
+  system.out.println(a);
+}
+}
