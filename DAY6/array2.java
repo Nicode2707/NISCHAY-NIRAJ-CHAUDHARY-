46872d7009c67936eee2e96e48b3d3b1af444ea9
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-07-28 13:11:24 +0530

    Create array2.java

---
+++ DAY6/array2.java
@@ -0,0 +1,17 @@
+public class arrt2 {
+
+    public static void update(int marks[]){
+        for (int i=0; i<marks.length; i++){
+            marks[i] = marks[i] + 1;
+        }
+    }
+    public static void main(String[] args) {
+        int marks[] = {87,94,86};
+         update(marks);
+        for (int i=0; i<marks.length; i++){
+            System.out.print(marks[i] + " " );
+        }
+        System.out.println();
+    }
+    }
+
