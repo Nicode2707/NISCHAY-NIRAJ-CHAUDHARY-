c165759fa846f37d14be7f7b0df7abaf9169d421
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-07-28 12:15:05 +0530

    Create Multi.java

---
+++ DAY5/Multi.java
@@ -0,0 +1,13 @@
+public class mult {
+
+    public static int multiply(int num1,int num2 ){
+        int multi = num1*num2;
+        return multi;
+    }
+    public static void main(String[] args) {
+        int a=4;
+        int b=6;
+        int prod = multiply(a,b);
+        System.out.println("product of a and b" + prod);
+    }
+}
