9fab41a33599dfd224c1942e0765918cbfe1a2a6
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-08-09 18:09:50 +0530

    Create stringfunctncomp.java

---
+++ DAY9/stringfunctncomp.java
@@ -0,0 +1,12 @@
+public class equalst {
+    public static void main(String[] args) {
+        String s1 ="tony";
+        String s3 = new String("tony");
+
+        if (s1.equals(s3)) {
+            System.out.println("string are equal");
+        } else {
+            System.out.println("string are not equal");
+        }
+    }
+}
