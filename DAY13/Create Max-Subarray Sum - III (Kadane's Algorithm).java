aea9e465e4d9ffead070b98ab12eaba2ae34b3d1
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-08-09 12:34:34 +0530

    Create Create Max-Subarray Sum - III (Kadane's Algorithm).java

---
+++ DAY13/Create Max-Subarray Sum - III (Kadane's Algorithm).java
@@ -0,0 +1,22 @@
+public class kadnes {
+    
+    public static void kadane(int numbers[]){
+        int maxsum = Integer.MIN_VALUE;
+        int currentsum =0;
+
+        for(int i=0; i<numbers.length;i++){
+            currentsum = currentsum + numbers[i];
+            if(currentsum<0){
+                currentsum =0;
+            }
+            maxsum = Math.max(currentsum,maxsum);
+        }
+
+        System.out.println("our max subarray sum is " + maxsum);
+    }
+
+    public static void main(String[] args) {
+       int numbers[] = {-2,-3,4,-2,1,5,-3}; 
+       kadane( numbers);
+    }  
+}
