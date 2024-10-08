f55dd60c1f581c767f31bc22e8a1aa8ebed2ff4b
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-10-08 12:52:14 +0530

    Create Deleteoperation.java

---
+++ Day23/Arraylist/Deleteoperation.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Delete {
+   public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+
+    list.add(5);
+    list.add(155);
+    list.add(25);
+    list.add(35);
+    list.add(45);
+    list.add(55);
+
+    System.out.println(list);
+
+    list.remove(2);
+    System.out.println(list);
+   } 
+}
