2c5173d3dcd05b1a164ea8a58df59cf0345a7b19
Niraj <155253763+nirajkr2707@users.noreply.github.com>
2024-10-09 12:16:17 +0530

    Create Reverseordersort.java

---
+++ DAY24/Reverseordersort.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class sort {
+   public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+
+    list.add(5);
+    list.add(155);
+    list.add(25);
+    list.add(35);
+    list.add(45);
+    list.add(55);
+
+    System.out.println(list);
+    //ascending order
+    Collections.sort(list); 
+    System.out.println(list);
+
+    //descending order
+    Collections.sort(list , Collections.reverseOrder());  //comprator - fnx logic
+   } 
+}
